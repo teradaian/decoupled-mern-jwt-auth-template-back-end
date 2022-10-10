39aef868e2abd83080d6352137bcf86c1c711442
teradaian <teradaian@gmail.com>
2022-10-10 06:36:04 -0700

    add create for new player

--- routes/scores.js
+++ routes/scores.js
@@ -4,11 +4,13 @@ import * as scoresCtrl from '../controllers/scores.js'
 const router = Router()
 
 /*---------- Public Routes ----------*/
+
 router.get('/', scoresCtrl.index)
 router.put('/increment/:id', scoresCtrl.update)
 router.put('/decrement/:id', scoresCtrl.updateDown)
 
 /*---------- Protected Routes ----------*/
+router.get('/add', scoresCtrl.add)
 
 
 export { router }
